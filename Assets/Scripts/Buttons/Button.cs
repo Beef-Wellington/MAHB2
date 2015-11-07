6ea21ba12213bf8ca89d653581957324addfb27c
Beef-Wellington <dylancet@gmail.com>
2015-11-07 15:42:05 -0500

    0.0.0
    Just the project

---
+++ Assets/Scripts/Buttons/Button.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//gets implemented into every ui Button ()
+public class Button : MonoBehaviour {
+
+	//implemented into action buttons (buttons that control Skinny [piss, drink beer, etc.])
+	public void actionButton (){
+		//Input.mousePosition;
+		Debug.Log("cik");
+	}
+}
