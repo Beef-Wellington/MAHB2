e6674ec1fb273ff2b480a2b7635e6b914e8274b0
Beef-Wellington <dylancet@gmail.com>
2015-11-08 23:49:43 -0500

    0.0.2
    Action buttons are conceptually correct

---
+++ Assets/Scripts/Buttons/ActionButton.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ActionButton : Button{
+
+	public Dropdown valueList;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	#region implemented abstract members of Button
+
+	public override void action (){
+
+	}
+
+	#endregion
+}
